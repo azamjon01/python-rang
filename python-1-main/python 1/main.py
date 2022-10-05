eee2d2f323a7a07eafcc09fead456cd6c97dc299
azamjon01 <110969340+azamjon01@users.noreply.github.com>
2022-10-05 14:32:14 +0500

    Add files via upload

---
+++ python-1-main/python 1/main.py
@@ -0,0 +1,22 @@
+age = 20
+vazn = 50.6
+ism = 'Anvar'
+uy_raqami = 65
+x = True
+
+print(type(age))
+print(type(vazn))
+print(type(ism))
+print(type(uy_raqami))
+print(type(x))
+
+ism = input("Ismingizni kiriting:")
+print('Salom ' + ism)
+
+ism = input("Ismingizni kiriting: ")
+rang = input("Yoqtirgan rangingizni kiriting:")
+print(ism + ' ' + rang + " rangni yoqtiradi")
+
+yil = input("Tug'ilgan yilingiz: ")
+yosh = 2022 - int(yil)
+print(yosh)
